2c393e2034139583bfc529d7086d7332c1cf2e51
junaid711 <155476114+junaid711@users.noreply.github.com>
2024-09-26 21:09:01 +0530

    added to script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function openMenu(event,menuName){
+	var i, tabcontent, tablinks;
+
+	tabcontent = document.getElementsByClassName('tabcontent');
+	for(i = 0; i < tabcontent.length; i++){
+		tabcontent[i].style.display = "none";
+	}
+
+
+	tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active","");
+    }
+
+	document.getElementById(menuName).style.display = "block";
+	event.currentTarget.className += " active";
+}
